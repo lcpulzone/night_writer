941cbe8735dd10da88a32845b4caf81128ce5fd5
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-06 14:46:57 -0800

    Update file to create any file put into command line

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,9 +5,31 @@ class NightWriter
     @input = ARGV
   end
 
-  if ARGV.length == 2
-    puts "Created 'braille.txt' containing 256 characters"
-  else
+  if ARGV.length == 2 && ARGV[0] == "message.txt"
+    puts "Created '#{ARGV[1]}' containing #{ARGV[1].length} characters"
+  elsif ARGV.length != 2
     puts "Please give two arguments for the proper message"
   end
+
+  handle = File.open(ARGV[0], "r+")
+
+  message_txt = handle.read
+
+  puts "- " * 25
+
+  def message_length
+     puts message_txt.length
+  end
+
+  puts message_txt
+
+  handle.close
+
+  writer = File.open(ARGV[1], "w+")
+
+  braille_txt = message_txt.replace
+
+  writer.write(braille_txt)
+
+  writer.close
 end
