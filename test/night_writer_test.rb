c75a7c1e0d7e803161cf2b5880b01fd4cf48e951
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-05 19:54:32 -0800

    Create assert_instance_of test

---
+++ test/night_writer_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+
+  def test_it_exists
+    night_writer = NightWriter.new
+    assert_instance_of NightWriter, night_writer
+  end
+end
