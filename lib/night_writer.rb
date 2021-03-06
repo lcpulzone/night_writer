f3c285cee9769f1a2f6c347e7ad40b08a3ce593f
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-05 21:43:39 -0800

    Create first iteration of ARGV message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,13 @@
 class NightWriter
+  attr_reader :input
 
+  def initialize
+    @input = ARGV
+  end
+
+  if ARGV.length == 2
+    puts "Created 'braille.txt' containing 256 characters"
+  else
+    puts "Please give two arguments for the proper message"
+  end
 end
