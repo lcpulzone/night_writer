02b94995b2365fb041fbc9d36ebfe5a20cea5e87
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-07 13:08:34 -0800

    Organize actions into methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,60 +1,67 @@
-require './lib/Library'
-
 class NightWriter
-  attr_reader :input
+  attr_reader :input,
+              :output
 
-  def initialize
-    @input = ARGV
-    library = Library.new
+  def initialize()
+    @input = ARGV[0]
+    @output = ARGV[1]
   end
 
-  if ARGV.length == 2 && ARGV[0] == "message.txt"
-    puts "Created '#{ARGV[1]}' containing #{ARGV[1].length} characters"
-  elsif ARGV.length != 2
-    puts "Please give two arguments for the proper message"
+  def first_message
+    file = File.open(input).read
   end
 
-  handle = File.open(ARGV[0], "r+")
-  message_txt = handle.read
-  puts "- " * 25
-  puts message_txt
-  puts "- " * 25
-  puts "There are #{message_txt.length} characters in the 'message_txt' file"
-  puts "- " * 25
-  handle.close
+  def write_message_to_file
+    second_message = first_message.replace("Oh no, the computer is starting to smoke...")
+    File.open(output, "w") do |file|
+      file.write(second_message)
+    end
+    puts "Created '#{output}' containing #{second_message.size} characters"
+    puts "- " * 25
+    puts first_message
+    puts "- " * 25
+    puts second_message.replace("Oh no, the computer is starting to smoke...")
+  end
 
-  writer = File.open(ARGV[1], "w+")
-  braille_txt = message_txt.replace("The computer is starting to smoke...")
-  writer.write(braille_txt)
-  writer.close
 end
-
-library = Library.new
-puts library.lower_case.values
-
-
-
-
-
-
-
+puts NightWriter.new.write_message_to_file
+# nr = NightWriter.new.read_message
 
 
 
 # class NightWriter
-#   def print_message
-#     puts "hi"
-#     file = File.open("message.txt").read
-#     puts file
+#   attr_reader :input,
+#               :output
+#
+#   def initialize
+#     @input = ARGV[0]
+#     @output = ARGV[1]
+#     library = Library.new
 #   end
 #
-#   def read_write(desired_file_path)
-#   message = "Is this really working??"
-#   File.open(desired_file_path, "a+") do |file|
-#     file.write(message)
+#   def first_message
+#     if @input.length == 2 && @input == "message.txt"
+#       puts "Created '#{@output}' containing #{@output.length} characters"
+#     elsif @input.length != 2
+#       puts "Please give two arguments for the proper message"
+#     end
 #   end
+#
+#   handle = File.open(ARGV[0], "r+")
+#   message_txt = handle.read
+#   puts "- " * 25
+#   puts message_txt
+#   puts "- " * 25
+#   puts "There are #{message_txt.length} characters in the 'message_txt' file"
+#   puts "- " * 25
+#   handle.close
+#
+#   writer = File.open(ARGV[1], "w+")
+#   braille_txt = message_txt.replace("The computer is starting to smoke...")
+#   writer.write(braille_txt)
+#   writer.close
 # end
 #
-# end
-# NightWriter.new.print_message
-# NightWriter.new.read_write('new_new.txt')
+# nightwriter = NightWriter.new.first_message
+# library = Library.new
+# puts library.lower_case.values
