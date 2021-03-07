c1a0459be30206a48ee30b2ca7d377a324ca5563
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-07 13:07:06 -0800

    Create translate method

--- lib/look_up.rb
+++ lib/look_up.rb
@@ -1,7 +1,18 @@
+require './lib/library'
+
 class LookUp
+  attr_reader :library
 
   def initialize
+    @library = Library.new
+  end
 
+  def translate(string)
+    # bucket = []
+    string.split("").map do |letter|
+      library.basic[letter]
+    end
+    # bucket
   end
 
 end
