5c83b44c8a7e02874eba3652dff255367cd7e397
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-08 09:59:29 -0800

    Update test to support three methods

--- test/look_up_test.rb
+++ test/look_up_test.rb
@@ -11,12 +11,14 @@ class LookUpTest < Minitest::Test
 
   def test_it_can_compare
     look_up = LookUp.new
-    assert_equal [["0.", "..", ".."]], look_up.translate("a")
+    assert_equal ["0.", "..", ".."], look_up.translate("a")
   end
 
-  def test_it_can_stack
+  def test_it_can_be_split
     look_up = LookUp.new
-    
-    assert_equal "0.\n..\n..\n", look_up.braille("a")
+    # require "pry";binding.pry
+    assert_equal "0.", look_up.line_one("a")
+    assert_equal "..", look_up.line_two("a")
+    assert_equal "..", look_up.line_three("a")
   end
 end
