7168ca9b52eba8bb64e5f783a8c241dabb78897b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-08 10:01:01 -0800

    Split method into three, beginning

--- lib/look_up.rb
+++ lib/look_up.rb
@@ -10,14 +10,11 @@ class LookUp
   def translate(string)
     string.split("").map do |letter|
       library.basic[letter]
-    end
+    end.flatten
   end
 
-  def braille(string)
-    sliced = translate(string).slice(0)
-    line1 = sliced.slice(0)
-    line2 = sliced.slice(1)
-    line3 = sliced.slice(2)
-    puts line1, line2, line3
+  def line_one(string)
+    line1 = translate(string).slice(0)
   end
+
 end
