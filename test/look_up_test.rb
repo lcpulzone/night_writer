06a2038a83a76b078f08875d0af41fe980cdf510
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-08 16:12:32 -0800

    Update test to pass

--- test/look_up_test.rb
+++ test/look_up_test.rb
@@ -11,12 +11,12 @@ class LookUpTest < Minitest::Test
 
   def test_it_can_compare
     look_up = LookUp.new
-    assert_equal ["0.", "..", ".."], look_up.translate("a")
+    assert_equal [["0.", "..", ".."]], look_up.translate("a")
   end
 
   def test_it_can_be_split
     look_up = LookUp.new
-    # require "pry";binding.pry
+
     assert_equal "0.", look_up.line_one("a")
     assert_equal "..", look_up.line_two("a")
     assert_equal "..", look_up.line_three("a")
